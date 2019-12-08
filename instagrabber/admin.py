cbb9c684fce0928e3f5f6bc641d9242ad13e5ce3
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-12-08 17:06:28 +0100

    Fix safe html

--- instagrabber/admin.py
+++ instagrabber/admin.py
@@ -26,13 +26,12 @@ class InstaUserAdmin(admin.ModelAdmin):
 
 
 from django.forms.utils import flatatt
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 class ImageUrlWidget(forms.TextInput):
     def render(self, name, value, attrs=None):
         forms.TextInput.render(self, name, value, attrs)
         flat_attrs = flatatt(attrs)
-        html = '<img src="%s" width="300" height="300"/>' % value
-        return mark_safe(html)
+        return format_html('<img src="%s" width="300" height="300"/>' % value)
 
 class InstaPictureForm(forms.ModelForm):
     class Meta:
@@ -74,6 +73,6 @@ class InstaPictureAdmin(admin.ModelAdmin):
         }
 
     def image_img(self, obj):
-        return u'<img class="instagram-picture" src="%s" width="150" height="150" title="%s"/>' % (obj.instagram_url, obj.caption)
+        return format_html('<img class="instagram-picture" src="%s" width="150" height="150" title="%s"/>' % (obj.instagram_url, obj.caption))
     image_img.short_description = 'Image'
     image_img.allow_tags = True
